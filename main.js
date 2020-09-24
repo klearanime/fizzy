d6d575924c3091b6ae441aac435ed9af6938ddfc
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-09-24 12:24:17 -0400

    one-off

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 function fizzy(num) {
-
+ if (num / 3 !==0 && num % 5 !==0) {
+   return 'Fizz'
+ } else if(num / 5 && num % 3) {
+   return 'Buzz'
+ } else if (num / 3 && num / 5) {
+   return 'FizzBuzz'
+  } else {
+  return 1
+ }
 }
+
+
+
 
 module.exports = {
   fizzy,
